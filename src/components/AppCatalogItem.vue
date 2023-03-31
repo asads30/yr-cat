3a4ccd4e04f775cfdfd7284c26b02ae2d8ad3bf0
asads30 <itmasteruz@yandex.com>
2023-03-31 20:26:50 +0500

    02

--- src/components/AppCatalogItem.vue
+++ src/components/AppCatalogItem.vue
@@ -36,17 +36,6 @@ export default {
     } catch (error) {
       console.log(error)
     }
-  },
-  methods: {
-    background (buffer) {
-      var binary = '';
-      var bytes = new Uint8Array( buffer );
-      var len = bytes.byteLength;
-      for (var i = 0; i < len; i++) {
-          binary += String.fromCharCode( bytes[ i ] );
-      }
-      return 'data:image/png;base64,' + window.btoa( binary )
-    }
   }
 }
 </script>
--- src/components/AppCatalogProduct.vue
+++ src/components/AppCatalogProduct.vue
@@ -59,6 +59,15 @@ export default {
     fetchData(){
       let current = this.$store.state.cart.find(product => product.id === this.product.id)?.quantity || null
       this.quantity = current
+    },
+    background (buffer) {
+      var binary = '';
+      var bytes = new Uint8Array( buffer );
+      var len = bytes.byteLength;
+      for (var i = 0; i < len; i++) {
+          binary += String.fromCharCode( bytes[ i ] );
+      }
+      return 'data:image/png;base64,' + window.btoa( binary )
     }
   },
   mounted() {
