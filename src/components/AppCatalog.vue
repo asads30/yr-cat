0e74c5bed7d7f7004fdcd7a651685ed6930eda28
asads30 <itmasteruz@yandex.com>
2023-03-31 18:51:16 +0500

    01

--- src/components/AppCatalog.vue
+++ src/components/AppCatalog.vue
@@ -2,24 +2,29 @@
     <div class="catalog">
         <div class="header">
           <b-nav pills class="header-nav" slot="header" v-b-scrollspy:nav-scroller>
-            <b-nav-item href="#cat1" @click="scrollIntoView">Кроссовки</b-nav-item>
-            <b-nav-item href="#cat2" @click="scrollIntoView">Толстовки</b-nav-item>
+            <b-nav-item 
+              v-for="category in categories"
+              :key="category.id"
+              :href="'#cat' + category.id" 
+              @click="scrollIntoView"
+            >
+              {{ category.name }}
+            </b-nav-item>
           </b-nav>
         </div>
         <div 
+          v-for="category in categories"
+          :key="category.id"
           id="nav-scroller"
           ref="content"
           class="catalog-content"
         >
-            <div class="catalog-box" id="cat1">
-                <div class="catalog-title">Кроссовки</div>
-                <div class="catalog-des">Описание категории, самые лучшие кроссовки, в описание вмещается до 256 символов</div>
-                <div class="catalog-list">
-                </div>
-            </div>
-            <div class="catalog-box" id="cat2">
-                <div class="catalog-title">Толстовки</div>
-                <div class="catalog-des">Описание категории, самые лучшие кроссовки, в описание вмещается до 256 символов</div>
+            <div
+              class="catalog-box"
+              :id="'cat' + category.id"
+            >
+                <div class="catalog-title">{{ category.name }}</div>
+                <div class="catalog-des">{{ category.des }}</div>
                 <div class="catalog-list">
                 </div>
             </div>
@@ -59,17 +64,17 @@
       loadData(){
         const id = this.$route.params.id;
         localStorage.setItem('id_store', id);
-        const tg = window.Telegram.WebApp;
-        tg.isClosingConfirmationEnabled = true;
-        tg.expand();
-        tg.BackButton.hide();
-        tg.MainButton.setParams({
-            color: '#27ae60',
-            text_color: '#fff'
-        });
-        localStorage.setItem('init_data', tg?.initData);
-        localStorage.setItem('user_id', tg?.initDataUnsafe.user.id);
-        console.log(tg);
+        // const tg = window.Telegram.WebApp;
+        // tg.isClosingConfirmationEnabled = true;
+        // tg.expand();
+        // tg.BackButton.hide();
+        // tg.MainButton.setParams({
+        //     color: '#27ae60',
+        //     text_color: '#fff'
+        // });
+        // localStorage.setItem('init_data', tg?.initData);
+        // localStorage.setItem('user_id', tg?.initDataUnsafe.user.id);
+        // console.log(tg);
       }
     },
     mounted() {
