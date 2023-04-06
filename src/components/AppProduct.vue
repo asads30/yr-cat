1c2a68d43076783dedc06a66d35b13e65cb39bee
asads30 <itmasteruz@yandex.com>
2023-04-06 15:41:57 +0500

    22

--- src/components/AppProduct.vue
+++ src/components/AppProduct.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="product" v-if="product">
-        {{ product }}
+        <div class="product-box">
+            <div :style="'background-image: url(' + background(product.thumbnail_buffer.data) + ');'" class="product-img"></div>
+            <div class="product-info">
+                <div class="product-header">
+                    <div class="product-name">{{ product.name }}</div>
+                    <div class="product-des">{{ product.description }}</div>
+                </div>
+                <div class="product-footer">
+                    <button v-if="btnActive" class="item-add" type="button" @click="addToCart">{{ product.price/100 }} ₽</button>
+                    <div v-if="!btnActive" class="quantity">
+                        <button class="quantity-item quantity-item--minus" type="button" @click="reduceQuantity"></button>
+                    <div class="quantity-val">{{ quantity }}</div>
+                        <button class="quantity-item quantity-item--plus" type="button" @click="increaseQuantity"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </template>
   
