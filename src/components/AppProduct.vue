a12e5ae9c8372cdff05039b150911045ee4f7105
asads30 <itmasteruz@yandex.com>
2023-04-06 14:56:11 +0500

    09

--- src/components/AppProduct.vue
+++ src/components/AppProduct.vue
@@ -6,7 +6,7 @@
       <div class="item-info">
         <div class="item-name">{{ product.name }}</div>
         <div class="item-des">{{ product.description }}</div>
-        <button v-if="btnActive" class="item-add" type="button" @click="addToCart">{{ getPrice.toLocaleString }} ₽</button>
+        <button v-if="btnActive" class="item-add" type="button" @click="addToCart">{{ product.price/100 }} ₽</button>
         <div v-if="!btnActive" class="quantity">
           <button class="quantity-item quantity-item--minus" type="button" @click="reduceQuantity"></button>
           <div class="quantity-val">{{ quantity }}</div>
@@ -22,54 +22,56 @@
         name: "AppCatalogProduct",
         data() {
             return {
-                quantity: 0
+                quantity: 0,
+                product: null
             }
         },
         computed: {
             btnActive() {
                 return this.$store.state.cart.find(product => product.id === this.product.id) ? this.product.isBtnActive : true;
-            },
-            getPrice(){
-                return this.product.price/100
             }
         },
         methods: {
-    addToCart() {
-      this.$store.commit('addToCart', this.product);
-      this.quantity++
-    },
-    reduceQuantity() {
-      this.$store.commit('reduceQuantity', this.product.id);
-      this.quantity--
-    },
-    increaseQuantity() {
-      this.$store.commit('increaseQuantity', this.product.id);
-      this.quantity++
-    },
-    fetchData(){
-      let current = this.$store.state.cart.find(product => product.id === this.product.id)?.quantity || null
-      this.quantity = current
-    },
-    background (buffer) {
-      var binary = '';
-      var bytes = new Uint8Array( buffer );
-      var len = bytes.byteLength;
-      for (var i = 0; i < len; i++) {
-          binary += String.fromCharCode( bytes[ i ] );
-      }
-      return 'data:image/png;base64,' + window.btoa( binary )
-    },
-    async fetchProduct(){
-        const id = this.$route.params.id
-
-    }
-  },
-  mounted() {
-    this.fetchData()
-    this.fetchProduct()
-  },
-  watch: {
-    $route: 'fetchData'
-  }
+            addToCart() {
+                this.$store.commit('addToCart', this.product);
+                this.quantity++
+            },
+            reduceQuantity() {
+                this.$store.commit('reduceQuantity', this.product.id);
+                this.quantity--
+            },
+            increaseQuantity() {
+                this.$store.commit('increaseQuantity', this.product.id);
+                this.quantity++
+                },
+            fetchData(){
+                let current = this.$store.state.cart.find(product => product.id === this.product.id)?.quantity || null
+                this.quantity = current
+            },
+            background (buffer) {
+                var binary = '';
+                var bytes = new Uint8Array( buffer );
+                var len = bytes.byteLength;
+                for (var i = 0; i < len; i++) {
+                    binary += String.fromCharCode( bytes[ i ] );
+                }
+                return 'data:image/png;base64,' + window.btoa( binary )
+            }
+        },
+        mounted() {
+            this.fetchData()
+            const id_store = localStorage.getItem('id_store')
+            const product_id = this.$route.params.id
+            try {
+                api.get(`/product/${id_store}/products/${product_id}`).then((response) => {
+                    this.product = response.data
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        watch: {
+            $route: 'fetchData'
+        }
   }
   </script>
